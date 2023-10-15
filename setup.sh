2352763d15430a1cf498b515fb545db659762d56
Sully <sullivaninus@gmail.com>
2023-10-15 16:10:25 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -76,7 +76,8 @@ if [ "$OS" == "Linux" ]; then
     echo "$counter. Installing zsh-extensions: auto-suggestions, syntax-highlighting..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh_themes
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
     ((counter++))
 
 
